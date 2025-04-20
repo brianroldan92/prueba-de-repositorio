7ddb52e196559af08d459db66d8aece35b979737
unknown <brianroldan1992@gmail.com>
2025-04-20 13:47:31 -0300

    tipos de variables

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 let titulo = document.getElementById ("titulo")
-titulo.textContent = "nuevo titulo"
+titulo.textContent = "Variables y tipos de datos";
 
+let altura = 1500
+let calle = "Av Rivadavia"
+let caba = true
+
+console.log (altura, typeof altura)
+console.log (calle, typeof calle)
+console.log (caba, typeof caba)
